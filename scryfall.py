86bb1d84475b38dc66b5ae96922178dbfcb6dc81
Louis Hagenbucher <louis.hagenbucher@gmail.com>
2023-12-12 21:42:36 +0900

    All call function
    Created a function that calls all pages from the api call. Needs optimization as it takes ages to complete the full call

--- scryfall.py
+++ scryfall.py
@@ -7,3 +7,19 @@ def call():
         data = req.json()
 
     return data
+
+def call_all(url='https://api.scryfall.com/cards/search?as=grid&order=name&q=%28game%3Apaper%29+legal%3Acommander'):
+    all_cards = []
+
+    while url:
+        req = requests.get(url)
+
+        if req.status_code == 200:
+            data = req.json()
+            all_cards.extend(data.get('data', []))
+            url = data.get('next_page')
+        else:
+            print(f"Error: {req.status_code}")
+            break
+
+    return all_cards
